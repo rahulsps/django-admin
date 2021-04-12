a23c2d48b49fbcecb4200d159bf5923c0b491496
dheerajsps <dheeraj.17softprodigy@gmail.com>
2021-04-12 13:07:34 +0530

    added testing.yml files

--- jangoadmin/tests/test_views.py
+++ jangoadmin/tests/test_views.py
@@ -1,6 +1,9 @@
 from .test_setup import TestSetUp 
 class TestView(TestSetUp):
     '''
+    Developer: Dheeraj Singh Rajput
+    Remarks: Don't change anything without discussion 
+    
     '''
     def test_user_can_register_without_data(self):
         res=self.client.post(self.register_url)
@@ -10,12 +13,9 @@ class TestView(TestSetUp):
     def test_user_can_register(self):
         res=self.client.post(self.register_url,self.user_data,format="json")
         self.assertEqual(res.status_code,201)
-
     def test_user_cannot_login_with_unknown_email(self):
         res=self.client.post(self.login_url)
         self.assertEqual(res.status_code,500)
     def test_user_can_login_with_correct_credentials(self):
         res=self.client.post(self.login_url,self.login_data,format="json")
-        self.assertEqual(res.status_code,200)
-
-    
+        self.assertEqual(res.status_code,200)    
